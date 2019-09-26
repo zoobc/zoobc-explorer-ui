537fa90cbfe2c2dc4668db9813222c02c051543c
I Gede Tirtanata <igedetirtanata@gmail.com>
2019-09-26 16:24:08 +0800

    update language shell script

--- language.sh
+++ language.sh
@@ -12,12 +12,19 @@ function echo_done() {
 }
 
 # ###### EXECUTES ######
-# 1. syncronize and update submodule zoobc-language-files
 SECONDS=0
-git submodule sync
-git submodule init
-git submodule update --init --recursive
+# 1. clone submodule zoobc-language-files
+git clone git@github.com:zoobc/zoobc-language-files.git
 
-# 2. finish
+# 2. copy repo zoobc-language-files
+if [ -d "./src/language/locales" ]; then
+  rm -rf "./src/language/locales"
+fi
+
+mkdir -p "./src/language"
+cp -R ./zoobc-language-files/locales ./src/language/locales/
+rm -rf "./zoobc-language-files"
+
+# 3. finish
 duration=$SECONDS
 echo -e "\n$(echo_done) Done in $(($duration % 60)) seconds."
