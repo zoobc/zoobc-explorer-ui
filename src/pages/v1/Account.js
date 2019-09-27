064655de62bb04a3019387e2fe967e0bcc409724
Irfan <sikumbangirfann@gmail.com>
2019-09-27 11:14:25 +0800

    implement api accounts (#25)

--- src/pages/v1/Account.js
+++ src/pages/v1/Account.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import { Row, Col, Card, Typography, Table, Pagination, Badge } from 'antd'
 import { Link } from 'react-router-dom'
+
 import DefaultLayout from '../../components/DefaultLayout'
 import Container from '../../components/Container'
 import DescItem from '../../components/DescItem'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
+import NotFound from '../../components/Errors/NotFound'
+import LoaderPage from '../../components/Loader/LoaderPage'
+import CopyToClipboard from '../../components/CopyToClipboard'
 
 const { Title } = Typography
+
+const GET_ACCOUNT_DATA = gql`
+  query getAccount($AccountAddress: String!) {
+    account(AccountAddress: $AccountAddress) {
+      AccountAddress
+      Balance
+      SpendableBalance
+      FirstActive
+      LastActive
+      TotalRewards
+      TotalFeesPaid
+      NodePublicKey
+    }
+  }
+`
 
 const columnsTrx = [
   {
@@ -66,43 +87,67 @@ const columnsNode = [
   },
 ]
 
-const Account = () => {
+const Account = ({ match }) => {
+  const { params, url } = match
+  const urlLastCharacter = url[url.length - 1]
+  let accountAddress = params.id
+
+  if (urlLastCharacter === '/') {
+    accountAddress = `${url.split('/')[2]}/`
+  }
+
+  const { loading, data, error } = useQuery(GET_ACCOUNT_DATA, {
+    variables: {
+      AccountAddress: accountAddress,
+    },
+  })
   return (
     <DefaultLayout>
-      <Container fluid>
-        <Row gutter={8}>
-          <Col span={24}>
-            <Row>
-              <Col span={24}>
-                <Title level={4}>Account #123456</Title>
-              </Col>
-            </Row>
-            <Card className="card-summary">
-              <Title level={4}>Summary</Title>
-              <DescItem label="Balance" value="200" />
-              <DescItem label="Pending Transactions" value="20" />
-              <DescItem label="Multisign Info" value="Bla bla bla" />
-              <DescItem label="Total Fee Paid" value="20" />
-              <DescItem label="Known Since" value="12-Jul-2017 12:00:00" />
-              <DescItem label="Last Active" value="1 week ago" />
-            </Card>
-            <Card className="card-summary">
-              <Title level={4}>
-                Node Info <Badge className="badge-black" count={425} overflowCount={1000} />
-              </Title>
-              <Table columns={columnsNode} dataSource={[]} pagination={false} size="small" />
-              <Pagination className="pagination-center" current={5} total={100} />
-            </Card>
-            <Card>
-              <Title level={4}>
-                Transactions <Badge className="badge-black" count={425} overflowCount={1000} />
-              </Title>
-              <Table columns={columnsTrx} dataSource={[]} pagination={false} size="small" />
-              <Pagination className="pagination-center" current={5} total={100} />
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      {!!error && <NotFound />}
+      {!!loading && <LoaderPage />}
+      {!error && !loading && (
+        <Container fluid>
+          <Row gutter={8}>
+            <Col span={24}>
+              <Row>
+                <Col span={24}>
+                  <Title level={4}>Account #{data.account.AccountAddress}</Title>
+                </Col>
+              </Row>
+              <Card className="card-summary">
+                <Title level={4}>Summary</Title>
+                <DescItem
+                  label="Account Address"
+                  value={
+                    <CopyToClipboard text={data.account.AccountAddress} keyID="accountAddress" />
+                  }
+                />
+                <DescItem label="Balance" value={data.account.Balance} />
+                <DescItem label="Spendable Balance" value={data.account.SpendableBalance} />
+                <DescItem label="First Active" value={data.account.FirstActive} />
+                <DescItem label="Last Active" value={data.account.LastActive} />
+                <DescItem label="Total Rewards" value={data.account.TotalRewards} />
+                <DescItem label="Total Fees Paid" value={data.account.TotalFeesPaid} />
+                <DescItem label="Node Public Key" value={data.account.NodePublicKey} />
+              </Card>
+              <Card className="card-summary">
+                <Title level={4}>
+                  Node Info <Badge className="badge-black" count={425} overflowCount={1000} />
+                </Title>
+                <Table columns={columnsNode} dataSource={[]} pagination={false} size="small" />
+                <Pagination className="pagination-center" current={5} total={100} />
+              </Card>
+              <Card>
+                <Title level={4}>
+                  Transactions <Badge className="badge-black" count={425} overflowCount={1000} />
+                </Title>
+                <Table columns={columnsTrx} dataSource={[]} pagination={false} size="small" />
+                <Pagination className="pagination-center" current={5} total={100} />
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      )}
     </DefaultLayout>
   )
 }
