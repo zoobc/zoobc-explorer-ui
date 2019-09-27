588b4dd5dcbcb01da6ca42f31eb99029f490805b
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-09-27 11:37:45 +0800

    implement node list page

--- src/pages/v1/Nodes.js
+++ src/pages/v1/Nodes.js
@@ -1,38 +1,122 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col, Card, Typography, Table, Pagination } from 'antd'
 import { Link } from 'react-router-dom'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
 import DefaultLayout from '../../components/DefaultLayout'
 import Container from '../../components/Container'
+import { shortenHash } from '../../utils/shorten'
+import CopyToClipboard from '../../components/CopyToClipboard'
 
 const { Title } = Typography
 
+const GET_NODES_DATA = gql`
+  query getNodes($page: Int) {
+    nodes(page: $page, limit: 15, order: "Height") {
+      Nodes {
+        NodePublicKey
+        OwnerAddress
+        NodeAddress
+        LockedFunds
+        RegistryStatus
+        ParticipationScore
+      }
+      Paginate {
+        Page
+        Count
+        Total
+      }
+    }
+  }
+`
+
 const columns = [
   {
-    title: 'IP Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: 'Node Public Key',
+    dataIndex: 'NodePublicKey',
+    key: 'NodePublicKey',
+    render(text, record) {
+      return (
+        <>
+          <Link to={`/nodes/${text}`}>{shortenHash(text, 30)}</Link>
+          <CopyToClipboard text={text} keyID={`Node-${record.key}`} showText={false} />
+        </>
+      )
+    },
+  },
+  {
+    title: 'Owner Address',
+    dataIndex: 'OwnerAddress',
+    key: 'OwnerAddress',
+    render(text, record) {
+      return (
+        <>
+          <Link to={`/nodes/${text}`}>{shortenHash(text, 30)}</Link>
+          <CopyToClipboard text={text} keyID={`Node-${record.key}`} showText={false} />
+        </>
+      )
+    },
+  },
+  {
+    title: 'Node Address',
+    dataIndex: 'NodeAddress',
+    key: 'NodeAddress',
     render(record) {
-      return <Link to="/">{record}</Link>
+      return !!record ? record : '-'
+    },
+  },
+  {
+    title: 'Locked Funds',
+    dataIndex: 'LockedFunds',
+    key: 'LockedFunds',
+    render(record) {
+      return !!record ? record : '-'
     },
   },
   {
     title: 'Registry Status',
-    dataIndex: 'lastActive',
-    key: 'lastActive',
+    dataIndex: 'RegistryStatus',
+    key: 'RegistryStatus',
+    render(record) {
+      return !!record.toString() ? (record.toString() === 'true' ? 'Registered' : 'In Queue') : '-'
+    },
   },
   {
-    title: 'PoP Score',
-    dataIndex: 'fees',
-    key: 'fees',
-  },
-  {
-    title: 'Owner',
-    dataIndex: 'known',
-    key: 'known',
+    title: 'Participation Score',
+    dataIndex: 'ParticipationScore',
+    key: 'ParticipationScore',
+    render(record) {
+      return !!record ? record : '-'
+    },
   },
 ]
 
 const Nodes = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [nodes, setNodes] = useState([])
+  const [paginate, setPaginate] = useState({})
+
+  const { loading, data } = useQuery(GET_NODES_DATA, {
+    variables: {
+      page: currentPage,
+    },
+  })
+
+  useEffect(() => {
+    if (!!data) {
+      const nodeData = data.nodes.Nodes.map((node, key) => {
+        return {
+          key,
+          ...node,
+        }
+      })
+
+      setNodes(nodeData)
+      setPaginate(data.nodes.Paginate)
+    }
+  }, [data])
+
   return (
     <DefaultLayout>
       <Container fluid>
@@ -42,13 +126,27 @@ const Nodes = () => {
               <Row>
                 <Col span={24}>
                   <Title level={4}>
-                    <i className="bcz-user" />
-                    Accounts
+                    <i className="bcz-node" />
+                    Nodes
                   </Title>
                 </Col>
               </Row>
-              <Table columns={columns} dataSource={[]} pagination={false} size="small" />
-              <Pagination className="pagination-center" current={5} total={100} />
+              <Table
+                columns={columns}
+                dataSource={nodes}
+                pagination={false}
+                size="small"
+                loading={loading}
+              />
+              {!!data && (
+                <Pagination
+                  className="pagination-center"
+                  current={paginate.Page}
+                  total={paginate.Total}
+                  pageSize={15}
+                  onChange={page => setCurrentPage(page)}
+                />
+              )}
             </Card>
           </Col>
         </Row>
