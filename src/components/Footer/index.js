b86bf940bb99190c6fcfcfe9de99105556f3a04d
I Gede Tirtanata <igedetirtanata@gmail.com>
2019-09-28 09:10:15 +0800

    footer new design

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Row, Col } from 'reactstrap'
 import { useTranslation } from 'react-i18next'
-
-import logo from '../../assets/images/zoobc.svg'
-// import languages from '../../languages'
-import SmallerDeviceFooter from './SmallerDeviceFooter'
+import { Switch } from 'antd';
 import Container from '../Container'
+
 
 export default function Footer() {
   const { t } = useTranslation()
+
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
   // const [lang, setLang] = useState('en')
 
   // const changeLanguage = e => {
@@ -18,65 +20,20 @@ export default function Footer() {
 
   return (
     <div className="footer">
-      <SmallerDeviceFooter className="d-md-none" />
-      <Container className="footer-body d-none d-md-block" fluid>
-        <Row className="footer-logo-row">
-          <Col className="col-md-12">
-            <div className="company-logo">
-              <a className="footer-brand" href="https://blockchainzoo.com/">
-                <img className="img-fluid" alt="logo" src={logo} />
-              </a>
-            </div>
-          </Col>
-        </Row>
-        <Row className="footer-text-row">
+      <Container className="footer-body">
+        <Row>
           <Col className="col-md-3">
-            <p className="footer-info-text">
-              {t(
-                'A webview for searching and displaying data published, so that a user can easily find any info about blockchain'
-              )}
-            </p>
-            <div className="footer-social-icons">
-              <a
-                className="footer-social-icon"
-                href="https://github.com/BlockchainZoo/spinechain-explorer-prototype"
-                rel="noopener noreferrer"
-                target="_blank"
-                title="GitHub"
-              >
-                <i className="fab fa-github mx-2" />
-              </a>
-              <a
-                className="footer-social-icon"
-                href="https://www.linkedin.com/company/blockchainzoo/"
-                rel="noopener noreferrer"
-                target="_blank"
-                title="LinkedIn"
-              >
-                <i className="fab fa-linkedin mx-2" />
-              </a>
-              <a
-                className="footer-social-icon"
-                href="https://www.youtube.com/channel/UCiFJtJ4gQX5P8Uh7_Onp4qQ"
-                rel="noopener noreferrer"
-                target="_blank"
-                title="Youtube"
-              >
-                <i className="fab fa-youtube mx-2" />
-              </a>
-              <a
-                className="footer-social-icon"
-                href="https://t.me/ZooBlockchain"
-                rel="noopener noreferrer"
-                target="_blank"
-                title="Telegram"
-              >
-                <i className="fab fa-telegram mx-2" />
-              </a>
-            </div>
+            <h3 className="footer-subtitle text-white heading-border">{t('ZooBC')}</h3>
+            <hr style={{ backgroundColor: '#E7EAF3', height: 0 }} />
+            <ul className="footer-list-group-item">
+              <p>&#169; 2019 ZooBC.com</p>
+              <p>All rights reserved.</p>
+              <p>v0.0.1-alpha.1</p>
+            </ul>
           </Col>
-          <Col className="col-md-3 footer-list">
+          <Col className="col-md-3">
             <h3 className="footer-subtitle text-white heading-border">{t('Product')}</h3>
+            <hr style={{ backgroundColor: '#E7EAF3', height: 0 }} />
             <ul className="footer-list-group mb-0">
               <a className="footer-list-group-item" href="https://blockchainzoo.com/">
                 {t('ZooBC Core')}
@@ -88,12 +45,13 @@ export default function Footer() {
                 {t('ZooBC Wallet')}
               </a>
               <a className="footer-list-group-item" href="https://blockchainzoo.com/">
-                {t('White Papper')}
+                {t('Whitepaper')}
               </a>
             </ul>
           </Col>
-          <Col className="col-md-3 footer-list">
+          <Col className="col-md-3">
             <h3 className="footer-subtitle text-white heading-border">{t('Community')}</h3>
+            <hr style={{ backgroundColor: '#E7EAF3', height: 0 }} />
             <ul className="footer-list-group mb-0">
               <a className="footer-list-group-item" href="https://blockchainzoo.com/">
                 {t('Getting Started')}
@@ -109,8 +67,9 @@ export default function Footer() {
               </a>
             </ul>
           </Col>
-          <Col className="col-md-3 footer-list">
+          <Col className="col-md-3">
             <h3 className="footer-subtitle text-white heading-border">{t('Company')}</h3>
+            <hr style={{ backgroundColor: '#E7EAF3', height: 0 }} />
             <ul className="footer-list-group mb-0">
               <a className="footer-list-group-item" href="https://blockchainzoo.com/">
                 {t('About Us')}
@@ -126,17 +85,72 @@ export default function Footer() {
               </a>
             </ul>
           </Col>
-          {/* <Col className="pr-0" md="4" lg="3">
-            <FormGroup className="d-flex align-items-center justify-content-right footer-nav py-1">
-              <Input type="select" value={lang} onChange={changeLanguage}>
-                {languages.map((language, key) => (
-                  <option key={key} value={language.value}>
-                    {language.label}
-                  </option>
-                ))}
-              </Input>
-            </FormGroup>
-          </Col> */}
+        </Row>
+        <hr style={{ backgroundColor: '#013558', height: 0, paddingLeft: '4px' }} />
+        <Row className="footer-ext">
+          <Switch className="btn-switch" />
+          <p style={{ marginTop: '6px', paddingLeft: '7px', color: '#fff' }}>Enable night mode</p>
+          <select style={{ marginLeft: '22px' }} defaultValue="english" onChange={handleChange}>
+            <option value="english">
+              English
+            </option>
+          </select>
+          <div className="footer-social-icons">
+            <a
+              className="footer-social-icon"
+              href="https://t.me/ZooBlockchain"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="Telegram"
+            >
+              <i className="fab fa-telegram mx-2" />
+            </a>
+            <a
+              className="footer-social-icon"
+              href="https://t.me/ZooBlockchain"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="Telegram"
+            >
+              <i className="fab fa-telegram mx-2" />
+            </a>
+            <a
+              className="footer-social-icon"
+              href="https://t.me/ZooBlockchain"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="Telegram"
+            >
+              <i className="fab fa-telegram mx-2" />
+            </a>
+            <a
+              className="footer-social-icon"
+              href="https://t.me/ZooBlockchain"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="Telegram"
+            >
+              <i className="fab fa-telegram mx-2" />
+            </a>
+            <a
+              className="footer-social-icon"
+              href="https://t.me/ZooBlockchain"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="Telegram"
+            >
+              <i className="fab fa-telegram mx-2" />
+            </a>
+            <a
+              className="footer-social-icon"
+              href="https://t.me/ZooBlockchain"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="Telegram"
+            >
+              <i className="fab fa-telegram mx-2" />
+            </a>
+          </div>
         </Row>
       </Container>
     </div>
