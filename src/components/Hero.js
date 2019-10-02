3e330c7d3ae7f9c2113bcbc99b68463bb1f803e0
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-10-02 19:25:09 +0800

    init searching feature

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
 import { Typography, Input, Row, Col, Card, Icon } from 'antd'
-
 const { Title } = Typography
 const { Search } = Input
 
-const Hero = () => {
+const GET_SEARCH_DATA = gql`
+  query getSearchData($Id: String!) {
+    search(Id: $Id) {
+      ID
+      Height
+      Timestamp
+      FoundIn
+    }
+  }
+`
+
+const Hero = ({ history }) => {
+  const [result, setResult] = useState({})
+
+  const { data } = useQuery(GET_SEARCH_DATA, {
+    variables: {
+      Id: result,
+    },
+  })
+
+  const searchHandler = value => {
+    setResult(value)
+
+    if (!!data) {
+      const { ID, FoundIn } = data.search
+      if (FoundIn === 'Block') {
+        return history.push(`/blocks/${ID}`)
+      } else if (data.search.FoundIn === 'Transaction') {
+        return history.push(`/transactions/${ID}`)
+      }
+    }
+  }
+
   return (
     <Card className="hero">
       <div className="hero-content">
@@ -16,6 +50,7 @@ const Hero = () => {
               prefix={<Icon type="search" style={{ fontSize: '22px', color: 'rgba(0,0,0,.45)' }} />}
               placeholder="Search by Address / Transaction ID / Block Height"
               enterButton="SEARCH"
+              onSearch={value => searchHandler(value)}
             />
           </Col>
         </Row>
@@ -28,4 +63,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default withRouter(Hero)
