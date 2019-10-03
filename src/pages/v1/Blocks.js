089c89397af9db38f886b60f5cfc3bc7d124ebcd
Eko Santoso <eksant@gmail.com>
2019-10-03 10:50:42 +0800

    [Blocks] Adding sorting per fields and fixing bugs header menus (#36)
    * Fixing search in header
    
    * [Blcoks] Adding sort per field
    
    * Restore input search
    
    * Restore styling active header menu
    
    * Add default sorting

--- src/pages/v1/Blocks.js
+++ src/pages/v1/Blocks.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import { Row, Col, Card, Typography, Table, Pagination } from 'antd'
+import { Row, Col, Card, Table, Pagination } from 'antd'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
 
+import { getSortString, isEmptyObject } from '../../utils'
 import DefaultLayout from '../../components/DefaultLayout'
 import Container from '../../components/Container'
 import { blockColumns } from '../../config/table-columns'
 
-const { Title } = Typography
-
+const defaultSort = { columnKey: 'Height', order: 'descend' }
 const GET_BLOCKS_DATA = gql`
-  query getBlocks($page: Int) {
-    blocks(page: $page, limit: 15, order: "-Height") {
+  query getBlocks($page: Int, $sorter: String) {
+    blocks(page: $page, limit: 15, order: $sorter) {
       Blocks {
         BlockID
         Height
@@ -33,10 +33,23 @@ const Blocks = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [blocks, setBlocks] = useState([])
   const [paginate, setPaginate] = useState({})
+  const [sorted, setSorted] = useState(defaultSort)
+
+  const onChangeTable = (pagination, filters, sorter) => {
+    setSorted(isEmptyObject(sorter) ? defaultSort : sorter)
+  }
+
+  const columns = blockColumns.map(item => {
+    item.sortDirections = ['descend', 'ascend']
+    item.sorter = (a, b) => a[item.dataIndex].length - b[item.dataIndex].length
+    item.sortOrder = sorted.columnKey === item.dataIndex && sorted.order
+    return item
+  })
 
   const { loading, data } = useQuery(GET_BLOCKS_DATA, {
     variables: {
       page: currentPage,
+      sorter: getSortString(sorted),
     },
   })
 
@@ -62,18 +75,20 @@ const Blocks = () => {
             <Card>
               <Row>
                 <Col span={24}>
-                  <Title level={4}>
+                  <h4>
                     <i className="bcz-calendar" />
                     Recent Blocks
-                  </Title>
+                  </h4>
                 </Col>
               </Row>
               <Table
-                columns={blockColumns}
+                // columns={blockColumns}
+                columns={columns}
                 dataSource={blocks}
                 pagination={false}
                 size="small"
                 loading={loading}
+                onChange={onChangeTable.bind(this)}
               />
               {!!data && (
                 <Pagination
