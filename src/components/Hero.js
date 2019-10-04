5e31c0825a6d94ac45f0b7b919d68faad2187859
Gede Yandi <43771081+gedeyandi456@users.noreply.github.com>
2019-10-04 11:28:33 +0800

    [Page] Search feature in Home Page (#39)
    * deleted language submodule
    
    * init searching feature
    
    * implement searching feature in home page
    
    * add AccountAddress filter in search bar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,32 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
 import { Input, Row, Col, Card, Icon } from 'antd'
+import LoaderPage from '../components/Loader/LoaderPage'
 
 const { Search } = Input
 
-const Hero = () => {
+const GET_SEARCH_DATA = gql`
+  query getSearchData($Id: String!) {
+    search(Id: $Id) {
+      ID
+      Height
+      Timestamp
+      FoundIn
+    }
+  }
+`
+
+const Hero = ({ history }) => {
+  const [keyword, setKeyword] = useState('')
+
+  const { loading, data, error } = useQuery(GET_SEARCH_DATA, {
+    variables: {
+      Id: keyword,
+    },
+  })
+
+  useEffect(() => {
+    if (!!data && !error && !loading) {
+      const { ID, FoundIn } = data.search
+      if (FoundIn === 'Block') history.push(`/blocks/${ID}`)
+      if (FoundIn === 'Transaction') history.push(`/transactions/${ID}`)
+      if (FoundIn === 'Account') history.push(`/accounts/${ID}`)
+    }
+  }, [keyword, data, loading, error, history])
+
   return (
     <Card className="hero">
-      <div className="hero-content">
-        <h3>
-          <strong>ZooBC Explorer</strong>
-        </h3>
-        <Row gutter={24} style={{ width: '100%' }}>
-          <Col span={24}>
-            <Search
-              size="large"
-              prefix={<Icon type="search" style={{ fontSize: '22px', color: 'rgba(0,0,0,.45)' }} />}
-              placeholder="Search by Address / Transaction ID / Block Height"
-              enterButton="SEARCH"
-            />
-          </Col>
-        </Row>
-        <h6 className="hero-subtitle">
-          A webview for searching and displaying data published, so that a user can easily find any
-          info about blockchain
-        </h6>
-      </div>
+      {!!loading && <LoaderPage />}
+      {!error && !loading && (
+        <div className="hero-content">
+          <h3>
+            <strong>ZooBC Explorer</strong>
+          </h3>
+          <Row gutter={24} style={{ width: '100%' }}>
+            <Col span={24}>
+              <Search
+                size="large"
+                prefix={
+                  <Icon type="search" style={{ fontSize: '22px', color: 'rgba(0,0,0,.45)' }} />
+                }
+                placeholder="Search by Account Address / Transaction ID / Block ID"
+                enterButton="SEARCH"
+                onSearch={value => setKeyword(value)}
+              />
+            </Col>
+          </Row>
+          <h6 className="hero-subtitle">
+            A webview for searching and displaying data published, so that a user can easily find
+            any info about blockchain
+          </h6>
+        </div>
+      )}
     </Card>
   )
 }
 
-export default Hero
+export default withRouter(Hero)
