853ef6677dca389e98dbaa91c5676dc657a8fd26
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-10-07 18:30:07 +0800

    return back graphql endpoint to server

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   endpoint: {
     api: 'http://139.162.15.80:6969/zoobc/api/v1',
-    graphql: 'http://localhost:6969/zoobc/api/v1/graphql',
+    graphql: 'http://139.162.15.80:6969/zoobc/api/v1/graphql',
   },
   app: {
     defaultLang: 'en',
