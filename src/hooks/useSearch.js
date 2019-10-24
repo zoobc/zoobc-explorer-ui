bacb9862535d32e870e4fbb79479bcf8a0e3ef68
Irfan <sikumbangirfann@gmail.com>
2019-10-24 22:16:06 +0800

    [Search] Adding not found search result notification (#56)
    * add not found search result notification
    
    * fix linter
    
    * avoid space in search input
    
    * fix linter
    
    * add not found page
    
    * change notfound wording
    
    * fix linter
    
    * fix wrong condition

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag'
-import { useQuery } from '@apollo/react-hooks'
+import { useLazyQuery } from '@apollo/react-hooks'
+import { useEffect } from 'react'
 
 const GET_SEARCH_DATA = gql`
   query getSearchData($Id: String!) {
@@ -12,14 +13,43 @@ const GET_SEARCH_DATA = gql`
   }
 `
 
-const useSearch = keyword => {
-  const { loading, data, error } = useQuery(GET_SEARCH_DATA, {
+const useSearch = (keyword, history) => {
+  const [doSearch, { loading, data, error, called }] = useLazyQuery(GET_SEARCH_DATA, {
     variables: {
       Id: keyword,
     },
   })
 
-  return { loading, error, data }
+  useEffect(() => {
+    if (!!data && !error && !loading) {
+      const { ID, FoundIn } = data.search
+      switch (FoundIn) {
+        case 'Block':
+          history.push(`/blocks/${ID}`)
+          break
+        case 'Transaction':
+          history.push(`/transactions/${ID}`)
+          break
+        case 'Account':
+          history.push(`/accounts/${ID}`)
+          break
+        default:
+          history.push({
+            pathname: '/search',
+            search: `?search=${keyword}`,
+            state: { search: keyword },
+          })
+          break
+      }
+    } else if (!!error) {
+      history.push({
+        pathname: '/not-found',
+        state: { keyword: keyword },
+      })
+    }
+  }, [keyword, data, loading, error, history])
+
+  return { loading, error, data, doSearch, called }
 }
 
 export default useSearch
