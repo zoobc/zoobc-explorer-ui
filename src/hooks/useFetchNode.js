f71ff172259c6f576919ce3d7e219ba0d528426f
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-11-05 02:09:13 +0800

    updated nodes page with 4 tabs

---
+++ src/hooks/useFetchNode.js
@@ -0,0 +1,37 @@
+import gql from 'graphql-tag'
+import { useLazyQuery } from '@apollo/react-hooks'
+import { getSortString } from '../utils'
+
+const GET_NODES_DATA = gql`
+  query getNodes($page: Int, $sorter: String, $tabValue: Int) {
+    nodes(page: $page, limit: 15, order: $sorter, RegistryStatus: $tabValue) {
+      Nodes {
+        NodePublicKey
+        OwnerAddress
+        NodeAddress
+        LockedFunds
+        RegistryStatus
+        ParticipationScore
+      }
+      Paginate {
+        Page
+        Count
+        Total
+      }
+    }
+  }
+`
+
+const useFetchNode = (page, sorted, tabValue) => {
+  const [doFetch, { loading, data, error, called }] = useLazyQuery(GET_NODES_DATA, {
+    variables: {
+      page: page,
+      sorter: getSortString(sorted),
+      tabValue: parseInt(tabValue),
+    },
+  })
+
+  return { loading, error, data, doFetch, called }
+}
+
+export default useFetchNode
