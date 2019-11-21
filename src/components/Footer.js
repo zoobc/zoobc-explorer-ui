7f7a81261996a3e75d978aca983554c63ff966c5
gedenata <igedetirtanata@gmail.com>
2019-11-21 11:04:15 +0800

    fixed hidden content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -75,9 +75,9 @@ const Footer = () => {
       <Collapse expandIconPosition="right">
         <Collapse.Panel header="Product" key="1">
           <ul className="footer-list-group mb-0">
-            {/* <a className="footer-list-group-item" onClick={onComingSoon}>
+            <a className="footer-list-group-item d-none" onClick={onComingSoon}>
               {t('ZooBC Core')}
-            </a> */}
+            </a>
             <a
               className="footer-list-group-item"
               target="_blank"
@@ -243,9 +243,9 @@ const Footer = () => {
               <h3 className="footer-subtitle heading-border">{t('Product')}</h3>
               <hr className="footer-horizontal-rule-light" />
               <ul className="footer-list-group mb-0">
-                {/* <a className="footer-list-group-item" href="#" onClick={onComingSoon}>
+                <a className="footer-list-group-item d-none" href="#" onClick={onComingSoon}>
                   {t('ZooBC Core')}
-                </a> */}
+                </a>
                 <a
                   className="footer-list-group-item"
                   target="_blank"
