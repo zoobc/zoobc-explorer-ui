8c3c91948bd129959da37fbcc4b6fc090fa12b52
eksant <eksant@gmail.com>
2020-07-28 18:22:02 +0800

    Adding apollo link to handle error

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,24 +1,58 @@
 import { WebSocketLink } from '@apollo/client/link/ws'
+import { onError } from '@apollo/client/link/error'
 import { getMainDefinition } from '@apollo/client/utilities'
-import { ApolloClient, split, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloLink, split, HttpLink, InMemoryCache } from '@apollo/client'
 
 import { testnetClient } from '../config/testnet'
+
+const createLink = uri => {
+  const httpLink = new HttpLink({ uri })
+
+  const wsLink = new WebSocketLink({
+    uri: uri
+      .replace('http', 'ws')
+      .replace('https', 'wss')
+      .replace('graphql', 'subscriptions'),
+    options: { reconnect: true, lazy: true, timeout: 20000 },
+  })
+
+  return split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    httpLink
+  )
+}
+
+const onErrorHandler = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    )
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+})
 
 const setupApolloCLient = uri => {
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: split(
-      ({ query }) => {
-        const definition = getMainDefinition(query)
-        return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-      },
-      new WebSocketLink({
-        // uri: 'ws://139.162.44.25:9090/zoobc/api/v1/graphql',
-        uri: uri.replace('http', 'ws').replace('https', 'wss'),
-        options: { reconnect: true, lazy: true, timeout: 20000 },
-      }),
-      new HttpLink({ uri })
-    ),
+    link: ApolloLink.from([onErrorHandler, createLink(uri)]),
+    // link: split(
+    //   ({ query }) => {
+    //     const definition = getMainDefinition(query)
+    //     return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+    //   },
+    //   new WebSocketLink({
+    //     // uri: 'ws://139.162.44.25:9090/zoobc/api/v1/graphql',
+    //     uri: uri
+    //       .replace('http', 'ws')
+    //       .replace('https', 'wss')
+    //       .replace('graphql', 'subscriptions'),
+    //     options: { reconnect: true, lazy: true, timeout: 20000 },
+    //   }),
+    //   new HttpLink({ uri })
+    // ),
   })
 }
 
