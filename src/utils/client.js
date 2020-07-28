da2d15ee161591e119bec760180624de4bf99439
eksant <eksant@gmail.com>
2020-07-28 18:22:31 +0800

    Adding apollo link to handle error

--- src/utils/client.js
+++ src/utils/client.js
@@ -38,21 +38,6 @@ const setupApolloCLient = uri => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: ApolloLink.from([onErrorHandler, createLink(uri)]),
-    // link: split(
-    //   ({ query }) => {
-    //     const definition = getMainDefinition(query)
-    //     return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-    //   },
-    //   new WebSocketLink({
-    //     // uri: 'ws://139.162.44.25:9090/zoobc/api/v1/graphql',
-    //     uri: uri
-    //       .replace('http', 'ws')
-    //       .replace('https', 'wss')
-    //       .replace('graphql', 'subscriptions'),
-    //     options: { reconnect: true, lazy: true, timeout: 20000 },
-    //   }),
-    //   new HttpLink({ uri })
-    // ),
   })
 }
 
