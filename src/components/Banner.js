29020c0f55e799dbc4bc4663d6d3e1602130940b
Irfan <sikumbangirfann@gmail.com>
2020-08-11 15:49:16 +0800

    Revert "Truncating the paragraph at "the" on the banner"
    This reverts commit b4648aba39af7e79ad989a193a6ed22e3828484c.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,7 @@
 import React from 'react'
 import Radium, { StyleRoot } from 'radium'
 import { slideInDown, fadeIn, flipInY } from 'react-animations'
+import { Col, Typography } from 'antd'
 import { isWebpSupported } from 'react-image-webp/dist/utils'
 
 const Banner = () => {
@@ -24,64 +25,68 @@ const Banner = () => {
 
   return (
     <StyleRoot>
-      <div className="banner">
-        <div className="banner-left">
-          <div style={styles.slideInDown}>
-            <div className="banner-title">
-              <strong>New to ZooBC ? Download the wallet to get started.</strong>
-            </div>
-            <div className="banner-text">
-              ZooBC wallet is a mobile apps, which can connect with the ZooBC blockchain network,
-              and allows you to do the following :
-            </div>
-            <div className="banner-sub-text">
-              <ul>
-                <li>Send ZooBC coins to your friends</li>
-                <li>See your balance and transaction history</li>
-                <li>Support Multi Signature Transaction</li>
-                <li>Support Escrow Transaction</li>
-              </ul>
-            </div>
-          </div>
-          <div className="banner-button" style={styles.fadeIn}>
-            <a
-              href="https://zoobc.app/zoobc-alpha.apk"
-              target="_blank"
-              rel="noopener norefferer"
-              title="PlayStore"
-            >
-              {isWebpSupported() ? (
-                <img src={require('../assets/images/googleplay.webp')} alt="googleplaystore" />
-              ) : (
-                <img src={require('../assets/images/googleplay.png')} alt="googleplaystore" />
-              )}
-            </a>
-            <a
-              href="https://testflight.apple.com/join/O1QFINJP"
-              target="_blank"
-              rel="noopener norefferer"
-              title="AppStore"
-            >
-              {isWebpSupported() ? (
-                <img src={require('../assets/images/appstore.webp')} alt="appstore" />
-              ) : (
-                <img src={require('../assets/images/appstore.png')} alt="appstore" />
-              )}
-            </a>
-          </div>
+      <Col className="home-col-left" md={{ span: 12 }} sm={{ span: 24 }}>
+        <h1 style={styles.slideInDown} className="banner-text">
+          <strong>New to ZooBC ?</strong>
+          <strong>
+            <p> Download the wallet to get started.</p>
+          </strong>
+          <p className="banner-text">
+            ZooBC wallet is a mobile application that is able to connect with the
+          </p>
+          <Typography className="banner-sub-text">
+            ZooBC blockchain network, which allows you to do the following :
+            <ul>
+              <li>Send ZooBC coins to your friends</li>
+              <li>See your balance and transaction history</li>
+              <li>Support Multi Signature Transaction</li>
+              <li>Support Escrow Transaction</li>
+            </ul>
+          </Typography>
+          <p className="banner-sub-text"></p>
+        </h1>
+
+        <div className="banner-button" style={styles.fadeIn}>
+          <a
+            className="play-store"
+            href="https://zoobc.app/zoobc-alpha.apk"
+            target="_blank"
+            rel="noopener norefferer"
+            title="PlayStore"
+          >
+            {isWebpSupported() ? (
+              <img src={require('../assets/images/googleplay.webp')} alt="googleplaystore" />
+            ) : (
+              <img src={require('../assets/images/googleplay.png')} alt="googleplaystore" />
+            )}
+          </a>
+          <a
+            href="https://testflight.apple.com/join/O1QFINJP"
+            target="_blank"
+            rel="noopener norefferer"
+            title="AppStore"
+          >
+            {isWebpSupported() ? (
+              <img src={require('../assets/images/appstore.webp')} alt="appstore" />
+            ) : (
+              <img src={require('../assets/images/appstore.png')} alt="appstore" />
+            )}
+          </a>
         </div>
-        <div className="banner-right">
-          {isWebpSupported() ? (
-            <img
-              src={require('../assets/images/banner.webp')}
-              alt="wallet"
-              style={styles.flipInY}
-            />
-          ) : (
-            <img src={require('../assets/images/banner.png')} alt="wallet" style={styles.flipInY} />
-          )}
-        </div>
-      </div>
+      </Col>
+
+      <Col
+        md={{ span: 12 }}
+        sm={{ span: 24 }}
+        style={{ textAlign: 'end' }}
+        className="home-col-right banner-wallet"
+      >
+        {isWebpSupported() ? (
+          <img src={require('../assets/images/banner.webp')} alt="wallet" style={styles.flipInY} />
+        ) : (
+          <img src={require('../assets/images/banner.png')} alt="wallet" style={styles.flipInY} />
+        )}
+      </Col>
     </StyleRoot>
   )
 }
