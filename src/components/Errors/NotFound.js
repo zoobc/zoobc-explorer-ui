98b1f921f41ae788cfd2e4efa14834174e8048d4
Irfan <sikumbangirfann@gmail.com>
2020-08-18 14:57:19 +0800

    fix blank page

--- src/components/Errors/NotFound.js
+++ src/components/Errors/NotFound.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import notFound from '../../assets/images/not-found.svg'
 import Container from '../Container'
+import { Button } from 'antd'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
     <Container>
       <div className="error-content">
         <img src={notFound} alt="not found" />
+        <span className="error-title">Sorry the page you are looking for is not found</span>
+        <Link to="/">
+          <Button type="primary">Back to Homepage</Button>
+        </Link>
       </div>
     </Container>
   )
