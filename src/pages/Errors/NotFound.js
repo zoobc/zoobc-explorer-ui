cd7a65b9b3cdfdba7565815a1a0fc8c9cebd92cd
suwidnyanaputra <grayfullbuster522@gmail.com>
2020-09-02 17:10:40 +0800

    change from light to dark not found page

--- src/pages/Errors/NotFound.js
+++ src/pages/Errors/NotFound.js
@@ -39,8 +39,8 @@ const NotFound = ({ history, location }) => {
     <Container>
       <div className="error-content error-content-page">
         <img src={searchResult} alt="not found" />
-        <span className="error-title">No result found</span>
-        <span className="h6 text-center">
+        <span className="error-title page-title">No result found</span>
+        <span className="h6 text-center page-title">
           {t("sorry, we couldn't find any results for")} {state.search}
         </span>
         <Search
