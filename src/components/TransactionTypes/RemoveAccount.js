eb8afab7f417e05a31a5cd312a525a16cff6f960
Irfan <sikumbangirfann@gmail.com>
2020-09-07 15:04:46 +0800

    fix linter

--- src/components/TransactionTypes/RemoveAccount.js
+++ src/components/TransactionTypes/RemoveAccount.js
@@ -7,8 +7,18 @@ const RemoveAccount = ({ data }) => {
   return (
     <Card className="transaction-card">
       <h4 className="transaction-card-title page-title">Remove Account</h4>
-      <DescItem label="Setter Address" style={{ display: 'none' }} value={data.NodeAddress} textClassName="monospace-text" />
-      <DescItem label="Recipient Address" style={{ display: 'none' }} value={data.AccountAddress} textClassName="monospace-text" />
+      <DescItem
+        label="Setter Address"
+        style={{ display: 'none' }}
+        value={data.NodeAddress}
+        textClassName="monospace-text"
+      />
+      <DescItem
+        label="Recipient Address"
+        style={{ display: 'none' }}
+        value={data.AccountAddress}
+        textClassName="monospace-text"
+      />
       <DescItem label="Property" style={{ display: 'none' }} value={data.Property} />
       <DescItem label="Value" style={{ display: 'none' }} value={data.Value} />
     </Card>
