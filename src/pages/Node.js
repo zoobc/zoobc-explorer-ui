dba146ea88428e25c2f83a443242a17bd2428df3
zaenury <adhiimzaenury@gmail.com>
2021-01-21 18:54:20 +0800

    Pagination quick jumper

--- src/pages/Node.js
+++ src/pages/Node.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * ZooBC Copyright (C) 2020 Quasisoft Limited - Hong Kong
  * This file is part of ZooBC <https://github.com/zoobc/zoobc-explorer-ui>
 
@@ -9,27 +9,27 @@
 
  * ZooBC is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
  * along with ZooBC.  If not, see <http://www.gnu.org/licenses/>.
 
  * Additional Permission Under GNU GPL Version 3 section 7.
- * As the special exception permitted under Section 7b, c and e, 
+ * As the special exception permitted under Section 7b, c and e,
  * in respect with the Author’s copyright, please refer to this section:
 
  * 1. You are free to convey this Program according to GNU GPL Version 3,
- *     as long as you respect and comply with the Author’s copyright by 
- *     showing in its user interface an Appropriate Notice that the derivate 
- *     program and its source code are “powered by ZooBC”. 
- *     This is an acknowledgement for the copyright holder, ZooBC, 
+ *     as long as you respect and comply with the Author’s copyright by
+ *     showing in its user interface an Appropriate Notice that the derivate
+ *     program and its source code are “powered by ZooBC”.
+ *     This is an acknowledgement for the copyright holder, ZooBC,
  *     as the implementation of appreciation of the exclusive right of the
  *     creator and to avoid any circumvention on the rights under trademark
  *     law for use of some trade names, trademarks, or service marks.
 
- * 2. Complying to the GNU GPL Version 3, you may distribute 
- *     the program without any permission from the Author. 
+ * 2. Complying to the GNU GPL Version 3, you may distribute
+ *     the program without any permission from the Author.
  *     However a prior notification to the authors will be appreciated.
 
  * ZooBC is architected by Roberto Capodieci & Barton Johnston
@@ -248,6 +248,7 @@ const Node = ({ match }) => {
                   />
                   {!!blocks && (
                     <Pagination
+                      showQuickJumper
                       className="pagination-center"
                       current={blockCurrentPage}
                       total={blockPaginate.Total}
