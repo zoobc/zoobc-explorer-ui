1298e5351bc36fc33d64e5bf984acf1efb964950
zaenury <adhiimzaenury@gmail.com>
2021-01-21 18:27:23 +0800

    eslint formatter

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -319,7 +319,7 @@ const Transaction = ({ match }) => {
                     bordered={false}
                     className="site-collapse-custom-collapse"
                     onChange={onChange}
-                    style={{background:'transparent'}}
+                    style={{ background: 'transparent' }}
                   >
                     <Panel showArrow={false} header={label} key="1" className="text-collapse">
                       <DescItem
